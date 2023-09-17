39c5e75720f3b5aaeab583c5a7ccb303e4cf946f
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-09-17 12:37:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name) {
+    const newCats = [...cats];
+    newCats.push(name);
+    return newCats;
+}
+
+function prependCat(name) {
+    const manyCats = [...cats];
+    manyCats.unshift(name);
+    return manyCats;
+}
+
+function removeLastCat() {
+    const lessCats = cats.slice(0,cats.length -1);
+    return lessCats;
+}
+
+function removeFirstCat() {
+    const fewCats = cats.slice(1);
+    return fewCats;
+}
